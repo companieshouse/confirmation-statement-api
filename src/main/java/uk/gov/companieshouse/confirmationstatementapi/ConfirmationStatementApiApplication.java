f6e7502aeaaa0e4b38b8e80d3643a2eba38a50f5
Lewis Evans <levans@companieshouse.gov.uk>
2021-04-27 10:27:31 +0100

    initial api application

---
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/ConfirmationStatementApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.confirmationstatementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationStatementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmationStatementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/confirmationstatementapi/ConfirmationStatementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.confirmationstatementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmationStatementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
