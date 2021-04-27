0674c29dca76848f59b08cd16fbccb5696bd71f7
kheseyongya <kheseyongya@gmail.com>
2021-04-27 11:42:20 +0100

    Add docker files to project

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
@@ -2,9 +2,11 @@ package uk.gov.companieshouse.confirmationstatementapi.contoller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/confirmation-statement")
 public class ConfirmationStatementController {
 
     @PostMapping("/")
