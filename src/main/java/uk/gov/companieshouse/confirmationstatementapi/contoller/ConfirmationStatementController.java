60fd3d1261efca48acd0e338ac921cca4d29ec18
kheseyongya <kheseyongya@gmail.com>
2021-04-27 11:14:15 +0100

    Clean up the pom file

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ConfirmationStatmentController {
+public class ConfirmationStatementController {
 
     @PostMapping("/")
     public ResponseEntity<String> createNewSubmission() {
