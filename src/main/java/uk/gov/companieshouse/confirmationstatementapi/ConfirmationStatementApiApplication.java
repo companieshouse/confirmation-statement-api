07787cd1b77a22d3214e867d4b385d5df849c009
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-04-29 15:12:45 +0100

    Add Api Logger

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/ConfirmationStatementApiApplication.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/ConfirmationStatementApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfirmationStatementApiApplication {
 
+	public static final String APP_NAMESPACE = "confirmation-statement-api";
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfirmationStatementApiApplication.class, args);
 	}
--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
@@ -1,21 +1,27 @@
 package uk.gov.companieshouse.confirmationstatementapi.contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
+import uk.gov.companieshouse.confirmationstatementapi.common.ApiLogger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/confirmation-statement")
 public class ConfirmationStatementController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger("Confirmation-Statement-Controller");
+    @Autowired
+    private ApiLogger apiLogger;
+
 
     @PostMapping("/")
     public ResponseEntity<String> createNewSubmission() {
-        LOGGER.info("'/confirmation-statement/' Endpoint hit");
+
+        apiLogger.info("POST '//confirmation-statement/' Endpoint hit");
         return ResponseEntity.ok().body("hello world");
     }
 }
