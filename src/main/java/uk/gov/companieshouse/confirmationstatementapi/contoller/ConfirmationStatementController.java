5c08086d308967038fc9507236ea0f74a30cf560
ICoxCH <ICox@companieshouse.gov.uk>
2021-05-10 14:50:33 +0100

    moved service to transactions based url

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/contoller/ConfirmationStatementController.java
@@ -1,6 +1,7 @@
 package uk.gov.companieshouse.confirmationstatementapi.contoller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,14 @@ import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
 @RestController
-@RequestMapping("/confirmation-statement")
+@RequestMapping("/transactions/{transaction_id}/confirmation-statement")
 public class ConfirmationStatementController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("Confirmation-Statement-Controller");
 
     @PostMapping("/")
-    public ResponseEntity<String> createNewSubmission() {
-        LOGGER.info("'/confirmation-statement/' Endpoint hit");
+    public ResponseEntity<String> createNewSubmission(@PathVariable("transaction_id") String transactionId) {
+        LOGGER.info("'/confirmation-statement/' Endpoint hit with id: " + transactionId);
         return ResponseEntity.ok().body("hello world");
     }
 }
