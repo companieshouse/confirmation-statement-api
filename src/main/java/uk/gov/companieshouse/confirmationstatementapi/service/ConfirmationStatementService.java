bbdf0fe425415b338777511011a4a2c9f4efa2d8
ICoxCH <ICox@companieshouse.gov.uk>
2021-05-12 13:26:11 +0100

    Log ineligible requests and reason

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/service/ConfirmationStatementService.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/service/ConfirmationStatementService.java
@@ -6,15 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.confirmationstatementapi.ConfirmationStatementApiApplication;
 import uk.gov.companieshouse.confirmationstatementapi.eligibility.EligibilityRule;
 import uk.gov.companieshouse.confirmationstatementapi.exception.EligibilityException;
 import uk.gov.companieshouse.confirmationstatementapi.exception.ServiceException;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 import java.net.URI;
 import java.util.List;
 
 @Service
 public class ConfirmationStatementService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfirmationStatementApiApplication.APP_NAME);
 
     private final CompanyProfileService companyProfileService;
 
@@ -35,6 +40,7 @@ public class ConfirmationStatementService {
                 eligibilityRule.validate(companyProfile);
             }
         } catch (EligibilityException e) {
+            LOGGER.info(String.format("Company %s ineligible to use the service because %s", transaction.getCompanyNumber(), e.getEligibilityFailureReason().toString()));
             return ResponseEntity.badRequest().body(e.getEligibilityFailureReason());
         }
 
