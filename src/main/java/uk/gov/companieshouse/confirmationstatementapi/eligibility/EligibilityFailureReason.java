dc811c6136c5f64bbcd11d2b08f9eb34de73b7f0
ICoxCH <ICox@companieshouse.gov.uk>
2021-05-17 12:16:45 +0100

    Updated paper response to PAPER_FILING_ONLY

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/EligibilityFailureReason.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/EligibilityFailureReason.java
@@ -3,6 +3,6 @@ package uk.gov.companieshouse.confirmationstatementapi.eligibility;
 public enum EligibilityFailureReason {
     INVALID_COMPANY_STATUS,
     INVALID_COMPANY_TYPE_CS01_FILING_NOT_REQUIRED,
-    INVALID_COMPANY_TYPE_PAPER_ONLY,
+    INVALID_COMPANY_TYPE_PAPER_FILING_ONLY,
     INVALID_COMPANY_TYPE_USE_WEB_FILING
 }
