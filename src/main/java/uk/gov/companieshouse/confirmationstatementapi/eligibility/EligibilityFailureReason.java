ebf536eec3f9156775691449a6196b2f515e468d
Lewis Evans <levans@companieshouse.gov.uk>
2021-05-17 08:08:32 +0100

    fix merge conflict

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/EligibilityFailureReason.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/EligibilityFailureReason.java
@@ -2,6 +2,6 @@ package uk.gov.companieshouse.confirmationstatementapi.eligibility;
 
 public enum EligibilityFailureReason {
     INVALID_COMPANY_STATUS,
-    INVALID_COMPANY_TYPE_CS01_FILING_NOT_REQUIRED
+    INVALID_COMPANY_TYPE_CS01_FILING_NOT_REQUIRED,
     INVALID_COMPANY_TYPE_PAPER_ONLY
 }
