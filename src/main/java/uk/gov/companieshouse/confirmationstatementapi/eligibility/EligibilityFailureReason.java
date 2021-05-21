83d1d16f1257b68aeb4bfeb62892aee26892bdd6
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-05-21 11:51:44 +0100

    Move PSCs validation into eligibility package.

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/EligibilityFailureReason.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/EligibilityFailureReason.java
@@ -1,6 +1,7 @@
 package uk.gov.companieshouse.confirmationstatementapi.eligibility;
 
 public enum EligibilityFailureReason {
+    COMPANY_HAS_MULTIPLE_PSCS,
     INVALID_COMPANY_STATUS,
     INVALID_COMPANY_TYPE_CS01_FILING_NOT_REQUIRED,
     INVALID_COMPANY_TYPE_PAPER_FILING_ONLY,
