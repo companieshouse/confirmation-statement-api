1f52af1defbac3fa7ac8a691bd6c85d3d3425f6a
ICoxCH <ICox@companieshouse.gov.uk>
2021-05-27 08:47:17 +0100

    Add logging to eligibility rules to allow debugging of requests

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/impl/CompanyStatusValidation.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/impl/CompanyStatusValidation.java
@@ -1,13 +1,18 @@
 package uk.gov.companieshouse.confirmationstatementapi.eligibility.impl;
 
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
+import uk.gov.companieshouse.confirmationstatementapi.ConfirmationStatementApiApplication;
 import uk.gov.companieshouse.confirmationstatementapi.eligibility.EligibilityStatusCode;
 import uk.gov.companieshouse.confirmationstatementapi.eligibility.EligibilityRule;
 import uk.gov.companieshouse.confirmationstatementapi.exception.EligibilityException;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 import java.util.Set;
 
 public class CompanyStatusValidation implements EligibilityRule<CompanyProfileApi> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfirmationStatementApiApplication.APP_NAME);
 
     private final Set<String> allowedStatuses;
 
@@ -17,10 +22,13 @@ public class CompanyStatusValidation implements EligibilityRule<CompanyProfileAp
 
     @Override
     public void validate(CompanyProfileApi profileToValidate) throws EligibilityException {
+        LOGGER.info("Validating Company Status for: " + profileToValidate.getCompanyNumber());
         var status = profileToValidate.getCompanyStatus();
 
         if (!allowedStatuses.contains(status)) {
+            LOGGER.info("Company Status validation failed for: " + profileToValidate.getCompanyNumber());
             throw new EligibilityException(EligibilityStatusCode.INVALID_COMPANY_STATUS);
         }
+        LOGGER.info("Company Status validation passed for: " + profileToValidate.getCompanyNumber());
     }
 }
