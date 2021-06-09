3eb1e66f78695f7e2ee0bf79684c293d941e8414
Lewis Evans <levans@companieshouse.gov.uk>
2021-06-09 08:36:06 +0100

    catch null pointer with 0 officer company

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/impl/CompanyOfficerValidation.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/eligibility/impl/CompanyOfficerValidation.java
@@ -46,10 +46,12 @@ public class CompanyOfficerValidation implements EligibilityRule<CompanyProfileA
 
     public int getOfficerCount(List<CompanyOfficerApi> officers) {
         int officerCount = 0;
-        for(CompanyOfficerApi officer: officers) {
-            var role = officer.getOfficerRole();
-            if ((role == OfficerRoleApi.DIRECTOR || role == OfficerRoleApi.NOMINEE_DIRECTOR || role == OfficerRoleApi.CORPORATE_DIRECTOR) && officer.getResignedOn() == null) {
-                officerCount++;
+        if (officers != null) {
+            for(CompanyOfficerApi officer: officers) {
+                var role = officer.getOfficerRole();
+                if ((role == OfficerRoleApi.DIRECTOR || role == OfficerRoleApi.NOMINEE_DIRECTOR || role == OfficerRoleApi.CORPORATE_DIRECTOR) && officer.getResignedOn() == null) {
+                    officerCount++;
+                }
             }
         }
         return officerCount;
