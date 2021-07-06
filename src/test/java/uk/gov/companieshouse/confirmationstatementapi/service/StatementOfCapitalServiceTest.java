a86dea90662b5db0f3314029875d1840da807788
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-07-06 10:37:23 +0100

    ncs 469 imports

--- src/test/java/uk/gov/companieshouse/confirmationstatementapi/service/StatementOfCapitalServiceTest.java
+++ src/test/java/uk/gov/companieshouse/confirmationstatementapi/service/StatementOfCapitalServiceTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import uk.gov.companieshouse.confirmationstatementapi.client.OracleQueryClient;
 import uk.gov.companieshouse.confirmationstatementapi.exception.ServiceException;
 import uk.gov.companieshouse.confirmationstatementapi.model.StatementOfCapital;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
