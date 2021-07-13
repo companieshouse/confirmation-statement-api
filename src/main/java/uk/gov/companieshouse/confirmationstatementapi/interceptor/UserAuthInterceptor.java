697bdd6ef47417ea6b10f9c75d573fd521b44e8a
ICoxCH <ICox@companieshouse.gov.uk>
2021-07-13 10:00:31 +0100

    remove unused imports from user auth interceptor

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/interceptor/UserAuthInterceptor.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/interceptor/UserAuthInterceptor.java
@@ -2,11 +2,8 @@ package uk.gov.companieshouse.confirmationstatementapi.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.ModelAndView;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
 
 import javax.servlet.http.HttpServletRequest;
