63f7b3239c5822d97503e4a8021ea7a9bbf78eec
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-08-23 14:38:39 +0100

    NCS-632 json proprty naming

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/model/json/payment/ConfirmationStatementPaymentJson.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/model/json/payment/ConfirmationStatementPaymentJson.java
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ConfirmationStatementPaymentJson {
 
-    @JsonProperty("paid")
-    private Boolean isPaid;
+    @JsonProperty("is_paid")
+    private Boolean paid;
 
     public Boolean isPaid() {
-        return isPaid;
+        return paid;
     }
 
-    public void setPaid(Boolean isPaid) {
-        this.isPaid = isPaid;
+    public void setPaid(Boolean paid) {
+        this.paid = paid;
     }
 }
 
