eef3b95dd481e32890cc8782574cd32e526fb40c
ICoxCH <ICox@companieshouse.gov.uk>
2021-08-25 11:22:35 +0100

    fix failing test

--- src/test/java/uk/gov/companieshouse/confirmationstatementapi/service/CostServiceTest.java
+++ src/test/java/uk/gov/companieshouse/confirmationstatementapi/service/CostServiceTest.java
@@ -1,9 +1,11 @@
 package uk.gov.companieshouse.confirmationstatementapi.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Collections;
 
@@ -15,6 +17,10 @@ class CostServiceTest {
     @InjectMocks
     private CostService costService;
 
+    @BeforeEach
+    void init() {
+        ReflectionTestUtils.setField(costService, "COST_AMOUNT", "13.00");
+    }
     @Test
     void getCosts() {
         var result = costService.getCosts();
