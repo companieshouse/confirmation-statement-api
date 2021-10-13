dc6c4d7ecdf5e3bbee45bb1ce1e6f8270dd7ecfc
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-10-13 12:27:08 +0100

    Remove unused import, and fix code smell.

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/interceptor/LoggingInterceptor.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/interceptor/LoggingInterceptor.java
@@ -4,7 +4,6 @@ import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
-import uk.gov.companieshouse.confirmationstatementapi.ConfirmationStatementApiApplication;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/utils/Constants.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/utils/Constants.java
@@ -2,6 +2,8 @@ package uk.gov.companieshouse.confirmationstatementapi.utils;
 
 public class Constants {
 
+    private Constants() {}
+
     public static final String ERIC_REQUEST_ID_KEY = "X-Request-Id";
     public static final String TRANSACTION_ID_KEY = "transaction_id";
     public static final String CONFIRMATION_STATEMENT_ID_KEY = "confirmation_statement_id";
