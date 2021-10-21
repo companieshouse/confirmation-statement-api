a55eb45f04203f575a9d3c0a347df2c1f3c90f56
kheseyongya <kheseyongya@gmail.com>
2021-10-21 12:09:37 +0100

    Change endpoint for active director to use transaction

--- src/test/java/uk/gov/companieshouse/confirmationstatementapi/controller/OfficerControllerTest.java
+++ src/test/java/uk/gov/companieshouse/confirmationstatementapi/controller/OfficerControllerTest.java
@@ -6,6 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.confirmationstatementapi.exception.ActiveDirectorNotFoundException;
 import uk.gov.companieshouse.confirmationstatementapi.exception.ServiceException;
 import uk.gov.companieshouse.confirmationstatementapi.model.ActiveDirectorDetails;
@@ -18,32 +19,35 @@ import static org.mockito.Mockito.when;
 class OfficerControllerTest {
 
     @Mock
+    private Transaction transaction;
+
+    @Mock
     private OfficerService officerService;
 
     @InjectMocks
     private OfficerController officerController;
 
-    private static final String COMPANY_NUMBER = "12345678";
     private static final String ERIC_REQUEST_ID = "XaBcDeF12345";
+    private static final String TRANSACTION_ID = "GFEDCBA";
 
     @Test
     void testGetActiveDirectorDetails() throws ActiveDirectorNotFoundException, ServiceException {
-        when(officerService.getActiveDirectorDetails(COMPANY_NUMBER)).thenReturn(new ActiveDirectorDetails());
-        var response = officerController.getActiveDirectorDetails(COMPANY_NUMBER, ERIC_REQUEST_ID);
+        when(officerService.getActiveDirectorDetails(transaction.getCompanyNumber())).thenReturn(new ActiveDirectorDetails());
+        var response = officerController.getActiveDirectorDetails(transaction, TRANSACTION_ID, ERIC_REQUEST_ID);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     void testGetActiveDirectorDetailsServiceException() throws ActiveDirectorNotFoundException, ServiceException {
-        when(officerService.getActiveDirectorDetails(COMPANY_NUMBER)).thenThrow(ServiceException.class);
-        var response = officerController.getActiveDirectorDetails(COMPANY_NUMBER, ERIC_REQUEST_ID);
+        when(officerService.getActiveDirectorDetails(transaction.getCompanyNumber())).thenThrow(ServiceException.class);
+        var response = officerController.getActiveDirectorDetails(transaction, TRANSACTION_ID, ERIC_REQUEST_ID);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 
     @Test
     void testGetActiveDirectorDetailsOfficerNotFoundException() throws ActiveDirectorNotFoundException, ServiceException {
-        when(officerService.getActiveDirectorDetails(COMPANY_NUMBER)).thenThrow(ActiveDirectorNotFoundException.class);
-        var response = officerController.getActiveDirectorDetails(COMPANY_NUMBER, ERIC_REQUEST_ID);
+        when(officerService.getActiveDirectorDetails(transaction.getCompanyNumber())).thenThrow(ActiveDirectorNotFoundException.class);
+        var response = officerController.getActiveDirectorDetails(transaction, TRANSACTION_ID, ERIC_REQUEST_ID);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
