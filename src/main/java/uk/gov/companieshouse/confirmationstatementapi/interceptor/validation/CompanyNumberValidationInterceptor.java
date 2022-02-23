cda25ae2872f8e50ac5949ae5f78161a034c6625
Matthew Culpin <mculpin@companieshouse.gov.uk>
2022-02-23 15:00:29 +0000

    BI-10599 Validate Company Number for eligibility and next made up to

---
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/interceptor/validation/CompanyNumberValidationInterceptor.java
@@ -0,0 +1,51 @@
+package uk.gov.companieshouse.confirmationstatementapi.interceptor.validation;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+import uk.gov.companieshouse.confirmationstatementapi.utils.ApiLogger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static uk.gov.companieshouse.confirmationstatementapi.utils.Constants.COMPANY_NUMBER;
+import static uk.gov.companieshouse.confirmationstatementapi.utils.Constants.ERIC_REQUEST_ID_KEY;
+
+@Component
+public class CompanyNumberValidationInterceptor implements HandlerInterceptor {
+    private static final Pattern COMPANY_NUMBER_PATTERN = Pattern.compile(
+            "^([a-z]|[a-z][a-z])?\\d{6,8}?$", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        final Map<String, String> pathVariables = (Map<String, String>) request
+                .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        String companyNumber = pathVariables.get(COMPANY_NUMBER);
+
+        ApiLogger.info(">>>>>>>>>>>>> PATH VARS " + pathVariables);
+        var reqId = request.getHeader(ERIC_REQUEST_ID_KEY);
+
+        if (StringUtils.isBlank(companyNumber)) {
+            ApiLogger.infoContext(reqId, "No company number supplied");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return false;
+        }
+
+        if (companyNumber.length() != 8) {
+            ApiLogger.infoContext(reqId, "Company number length is invalid");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return false;
+        }
+        Matcher matcher = COMPANY_NUMBER_PATTERN.matcher(companyNumber);
+        if(!matcher.find()){
+            ApiLogger.infoContext(reqId, "Company number contains invalid characters");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return false;
+        }
+        return true;
+    }
+}
--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/utils/Constants.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/utils/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String TRANSACTION_ID_KEY = "transaction_id";
     public static final String CONFIRMATION_STATEMENT_ID_KEY = "confirmation_statement_id";
     public static final String FILING_KIND_CS = "confirmation-statement";
+    public static final String COMPANY_NUMBER = "company-number";
 }
