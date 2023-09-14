dd137ebb6daf286b10ad0557c036ff4e46def959
pparlett <pparlett@companieshouse.gov.uk>
2023-09-14 16:59:23 +0100

    initial commit of changes for get rea request

---
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/service/EmailService.java
@@ -0,0 +1,23 @@
+package uk.gov.companieshouse.confirmationstatementapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uk.gov.companieshouse.confirmationstatementapi.client.OracleQueryClient;
+import uk.gov.companieshouse.confirmationstatementapi.exception.ServiceException;
+
+@Service
+public class EmailService {
+
+    private final OracleQueryClient oracleQueryClient;
+
+    @Autowired
+    public EmailService(OracleQueryClient oracleQueryClient) {
+        this.oracleQueryClient = oracleQueryClient;
+    }
+
+    public String getRegisteredEmailAddress(String companyNumber) throws ServiceException {
+        return "registered-email-address";
+    }
+
+}
