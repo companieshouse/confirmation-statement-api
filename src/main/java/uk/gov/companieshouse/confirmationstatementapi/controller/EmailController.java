b02734192371a368805ec6a689e71398ebd8f86d
adeharrison-ch <aharrison1@companieshouse.gov.uk>
2023-10-04 13:54:01 +0100

    Refactor mapping

--- src/main/java/uk/gov/companieshouse/confirmationstatementapi/controller/EmailController.java
+++ src/main/java/uk/gov/companieshouse/confirmationstatementapi/controller/EmailController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.confirmationstatementapi.exception.RegisteredEmailNotFoundException;
@@ -20,6 +21,7 @@ import static uk.gov.companieshouse.confirmationstatementapi.utils.Constants.ERI
 import static uk.gov.companieshouse.confirmationstatementapi.utils.Constants.TRANSACTION_ID_KEY;
 
 @RestController
+@RequestMapping("/transactions/{transaction_id}/confirmation-statement")
 public class EmailController {
 
     private final EmailService emailService;
@@ -29,7 +31,7 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @GetMapping("/transactions/{transaction_id}/confirmation-statement/{confirmation_statement_id}/registered-email-address")
+    @GetMapping("/{confirmation_statement_id}/registered-email-address")
     public ResponseEntity<String> getRegisteredEmailAddress(
             @RequestAttribute("transaction") Transaction transaction,
             @PathVariable(TRANSACTION_ID_KEY) String transactionId,
@@ -45,7 +47,7 @@ public class EmailController {
         } catch (RegisteredEmailNotFoundException renfe) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(renfe.getMessage());
         } catch (ServiceException e) {
-            ApiLogger.errorContext(requestId, "Error retrieving shareholders data", e, map);
+            ApiLogger.errorContext(requestId, "Error retrieving registered email address", e, map);
             return ResponseEntity.internalServerError().build();
         }
     }
