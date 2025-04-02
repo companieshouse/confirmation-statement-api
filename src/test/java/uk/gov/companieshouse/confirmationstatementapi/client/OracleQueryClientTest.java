1f742d7c44ac93a99424f00b4f4d323fdd116bca
GruffTrick <gtrick@companieshouse.gov.uk>
2025-04-02 14:22:29 +0100

    removed unused commit

--- src/test/java/uk/gov/companieshouse/confirmationstatementapi/client/OracleQueryClientTest.java
+++ src/test/java/uk/gov/companieshouse/confirmationstatementapi/client/OracleQueryClientTest.java
@@ -10,7 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestClientException;
 
