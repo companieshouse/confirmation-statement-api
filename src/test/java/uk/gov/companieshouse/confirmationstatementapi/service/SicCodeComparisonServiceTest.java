8b36de34d93870fb8bca1f9576c695e9311efad6
dho-ch <dho@companieshouse.gov.uk>
2026-03-19 16:21:54 +0000

    CSE-1064: fix sonarQube issue

--- src/test/java/uk/gov/companieshouse/confirmationstatementapi/service/SicCodeComparisonServiceTest.java
+++ src/test/java/uk/gov/companieshouse/confirmationstatementapi/service/SicCodeComparisonServiceTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @ExtendWith(MockitoExtension.class)
-public class SicCodeComparisonServiceTest {
+class SicCodeComparisonServiceTest {
 
     @InjectMocks
     private SicCodeComparisonService sicCodeComparisonService;
